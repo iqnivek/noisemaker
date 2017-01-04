39a28ccd5cb979b6ef2f12cbe68a52c33eef0826
Kevin Qi <iqnivek@gmail.com>
2017-01-03 21:20:11 -0800

    Initial working version based on http://noisehack.com/generate-noise-web-audio-api

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var audioContext = new AudioContext();
+
+var bufferSize = 4096;
+var brownNoise = (function() {
+    var lastOut = 0.0;
+    var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    node.onaudioprocess = function(e) {
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            var white = Math.random() * 2 - 1;
+            output[i] = (lastOut + (0.02 * white)) / 1.02;
+            lastOut = output[i];
+            output[i] *= 0.8; // (roughly) compensate for gain
+        }
+    }
+    return node;
+})();
+
+brownNoise.connect(audioContext.destination);
